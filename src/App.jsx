50cffb71ec939f0879f50f5f299e86e289a8057b
AT010303 <101562874+AT010303@users.noreply.github.com>
2024-09-16 14:29:15 +0530

    made some loader adjustment

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { ZapparCamera, InstantTracker, ZapparCanvas } from "./component/lib";
 import { Suspense } from "react";
 import { useAssetStore } from "./helper/assetStore";
 import { useProgress } from "@react-three/drei";
+import Loader from "./component/loader.jsx";
 
 function App() {
   let [placementMode, setPlacementMode] = useState(true);
@@ -26,40 +27,10 @@ function App() {
   return (
     <>
       {/* Display spinner while loading */}
-      {loadingStore.loading && (
-        <div
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-            backgroundColor: "rgba(0, 0, 0, 1)",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            zIndex: 9999,
-          }}
-        >
-          {/* Independent spinner */}
-          <div
-            style={{
-              border: "8px solid #f3f3f3", // Light grey background for spinner
-              borderRadius: "50%",
-              borderTop: "8px solid #00FF00", // Green top to animate
-              width: "50px",
-              height: "50px",
-              animation: "spin 1s linear infinite", // Rotation animation
-            }}
-          />
-          <div style={{ color: "white", fontSize: "12px", marginTop: "15px" }}>
-            Loading...
-          </div>
-        </div>
-      )}
+      {loadingStore.loading && <Loader/>}
 
       <ZapparCanvas>
+        
         <ZapparCamera />
         <InstantTracker
           placementMode={placementMode}
